5e2d91dd328dd3e7b7e14f2def895bf868851aa0
Victor Manuel <victorakiladev@gmail.com>
2024-05-16 22:05:28 -0500

    example

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -15,7 +15,6 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-		</ul>
 	</nav>
 	<Dropdown/>
 </header>
--- src/routes/orders/data.js
+++ src/routes/orders/data.js
@@ -1,40 +1,7 @@
-export const posts = [
-	{
-        "id": "1",
-        "title": "Ejemplo de pedido",
-        "status": "Pending",
-        "description": "Descripción del pedido",
-        "payment": 100.0,
-        "user": "1a",
-        "creation_date": null,
-        "end_date": null,
-        "availability_date": null,
-        "category": "Remote"
-    },
+export const login =  (login) => {
+    //aqui iria un fecth cuando agamos el api
+    console.log(login)
+    //esto seria el string o token que debe guadar
+    return "bearer"
+}
 
-	{
-        "id": "2",
-        "title": "Ejemplo de pedido",
-        "status": "Pending",
-        "description": "Descripción del pedido",
-        "payment": 100.0,
-        "user": "2b",
-        "creation_date": null,
-        "end_date": null,
-        "availability_date": null,
-        "category": "Remote"
-    },
-
-	{
-        "id": "3",
-        "title": "Ejemplo de pedido",
-        "status": "Pending",
-        "description": "Descripción del pedido",
-        "payment": 100.0,
-        "user": "3c",
-        "creation_date": null,
-        "end_date": null,
-        "availability_date": null,
-        "category": "Remote"
-    }
-];
